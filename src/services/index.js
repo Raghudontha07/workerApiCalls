b6301ee053d6200c9f1ef4c485a5ee8ae4178174
Raghupathi Dontha <raghupathi.dontha@berkadia.com>
2021-08-31 18:40:32 +0530

    refactored

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,1 @@
-// Copyright (C) 2018 Berkadia Commercial Mortgage, LLC - All Rights Reserved
 export * from './log';
-export * from './errorMessages.service';
--- src/services/log.js
+++ src/services/log.js
@@ -1,10 +1,8 @@
-// Copyright (C) 2018 Berkadia Commercial Mortgage, LLC - All Rights Reserved
 import Pino from 'express-pino-logger';
 
 // Add Middlewares
 export const LogMiddleware = Pino({
-  name: 'Workers_usage'
-  // , level: 'debug'
+  name: 'workersAPI'
 });
 
 export const Log = LogMiddleware.logger;
